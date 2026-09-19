f7ac462006c28b2552e9d0fbf11b08b39ec51447
oriolbg <joborrasg@gmail.com>
2026-09-19 12:28:56 -0300

    First commit

---
+++ apilogistica/src/main/java/erp/gestion/apilogistica/ApilogisticaApplication.java
@@ -0,0 +1,13 @@
+package erp.gestion.apilogistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApilogisticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApilogisticaApplication.class, args);
+	}
+
+}
